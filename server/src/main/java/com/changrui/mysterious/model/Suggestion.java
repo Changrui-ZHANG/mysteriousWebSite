fe5db4cbfeeadf5d44811fcf0c3604232d376517
Changrui <m.zhang.changrui@gmail.com>
2025-12-13 22:03:50 +0100

    suggestion fix

--- server/src/main/java/com/changrui/mysterious/model/Suggestion.java
+++ server/src/main/java/com/changrui/mysterious/model/Suggestion.java
@@ -21,6 +21,7 @@ public class Suggestion {
     private String suggestion;
 
     @Column(nullable = false)
+    @com.fasterxml.jackson.annotation.JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime timestamp;
 
     @Column(nullable = false)
